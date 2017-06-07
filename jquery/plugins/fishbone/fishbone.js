835af145690386403f8cf5e5406a7417a22d0868
znyaiw <znyaiw@zhushuangmei.local>
2017-06-07 18:59:58 +0800

    plugin fishbone

--- jquery/plugins/fishbone/fishbone.js
+++ jquery/plugins/fishbone/fishbone.js
@@ -31,28 +31,31 @@
         return this.drawFishBone(this.fishData);
       },
       drawFishBone: function(a) {
-        var top = $('<div class="fish-data"></div>'),
+        var main = $('<div class="fish-main"></div>'),
+            top = $('<div class="fish-data"></div>'),
             bottom = $('<div class="fish-data bottom"></div>'),
             tfrag = $(document.createDocumentFragment()),
             bfrag = $(document.createDocumentFragment()),
             tul = $('<ul class="fish-items"></ul>'),
-            bul = $('<ul class="fish-items"></ul>')
+            bul = $('<ul class="fish-items"></ul>'),
+            prev = $('<a href="#" class="fish-slide" data-slide="prev"><i class="fish-icon icon-arrow-left"></i></a>'),
+            next = $('<a href="#" class="fish-slide" data-slide="next"><i class="fish-icon icon-arrow-right"></i></a>'),
             tli = null;
             bli = null;
 
         for(var i = 0; i < a.length; i++) {
           if(a[i].position === 'top') {
             tli = $('<li class="item"></li>');
-            tli.html('<em></em><h3 class="item-title">' + a[i].title + '</h3><p class="item-content">' + a[i].content + '</p>');
-            tli.css('border-color', a[i].color);
-            tli.find('em').css('background-color', a[i].color);
+            tli.html('<em></em><h3 class="item-title">' + a[i].title + '</h3><p class="item-content">' + a[i].content + '</p>')
+              .css('border-color', a[i].color)
+              .find('em').css('background-color', a[i].color);
             tfrag.append(tli);
           }
           else {
             bli = $('<li class="item"></li>');
-            bli.html('<em></em><h3 class="item-title">' + a[i].title + '</h3><p class="item-content">' + a[i].content + '</p>');
-            bli.css('border-color', a[i].color);
-            bli.find('em').css('background-color', a[i].color);
+            bli.html('<em></em><h3 class="item-title">' + a[i].title + '</h3><p class="item-content">' + a[i].content + '</p>')
+              .css('border-color', a[i].color)
+              .find('em').css('background-color', a[i].color);
             bfrag.append(bli);
           }
         }
@@ -60,18 +63,32 @@
         bul.append(bfrag);
         top.append(tul);
         bottom.append(bul);
-        this.el.append(top, $('<div class="fish-line"></div>'), bottom);
-        this.el.addClass('js-fishbone');
+        main.append(top, $('<div class="fish-line"></div>'), bottom, prev, next);
+        this.el.append(main).addClass('js-fishbone');
+
+        var _ = this;
+        $('.js-fishbone .item').hover(function() {
+          $(this).css('border-color', _.settings.hovers[0])
+            .find('em').css('background-color', _.settings.hovers[0]);
+        }, function() {
+          $(this).css('border-color', _.settings.colors[0])
+            .find('em').css('background-color', _.settings.colors[0]);
+        });
+
+        $('.js-fishbone .fish-slide').hover(function() {
+          console.log('hover');
+        })
 
         return this.reasonably();
       },
       reasonably: function() {
         var perWidth = ($('.js-fishbone').parent().width() - 20 * this.settings.row) / this.settings.row;
-        $('.js-fishbone .item').css('width', perWidth);
-        $('.js-fishbone .fish-items').css('width', Math.ceil(this.fishData.length / 2) * (perWidth + 20));
-        $('.js-fishbone .fish-line').css('width', Math.ceil(this.fishData.length / 2) * (perWidth + 20) + (this.settings.mode === 'cross' ? perWidth / 2 : 0));
+        $('.js-fishbone').find('.item').css('width', perWidth).end()
+          .find('.fish-items').css('width', Math.ceil(this.fishData.length / 2) * (perWidth + 20)).end()
+          .find('.fish-line').css({'width': Math.ceil(this.fishData.length / 2) * (perWidth + 20) + (this.settings.mode === 'cross' ? perWidth / 2 : 0), 'border-style': this.settings.line }).end();
+
         if(this.settings.mode === 'cross') {
-          $('.js-fishbone .fish-data.bottom').css('margin-left', perWidth / 2 + 10);
+          $('.js-fishbone .fish-data.bottom').css('margin-left', perWidth / 2);
         }
       }
     }
@@ -107,12 +124,14 @@
       { title: '2014/02/05', content: 'Launch of oBike in Singapore' },
       { title: '2014/02/06', content: 'Launch of oBike in Singapore' },
       { title: '2014/03', content: 'Launch of oBike in Singapore' },
-      { title: '2014/02/03', content: 'Launch of oBike in Singapore' },
+      { title: '2014/02/03', content: 'Launch of oBike in Singapore Launch of oBike in Singapore Launch of oBike in Singapore Launch of oBike in Singapore Launch of oBike in Singapore' },
       { title: '2014/03/05', content: 'Launch of oBike in Singapore' },
       { title: '2014/03/06', content: 'Launch of oBike in Singapore' },
     ],
-    colors: ['#F44336', '#9C27B0', '#2196F3', '#4CAF50'],
+    colors: ['#fff'],
+    hovers: ['#ddd'],
     row: 3,
+    line: 'solid',
     mode: 'cross'
   };
   // 添加一个自动调用的方法，在需要调用的标签里面添加括号里面的内容
